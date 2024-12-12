2b2c508da49d4a6321dd23dc8d2e34267a78c439
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2024-12-12 17:59:22 +0200

    Draw Font test

--- font.s
+++ font.s
@@ -1,5 +1,215 @@
+	
+	IMPORT DRAW_RECTANGLE_FILLED
+	
+	EXPORT DRAW
+	
+	
+	AREA MYCODE, CODE, READONLY
+		
 
 
+
+DRAW FUNCTION
+	; Draws ASCII character in R2 starting from the top left corner x, y:
+	; R0: x
+	; R1: y
+	; R2: ASCII character
+	; R10: color
+	PUSH {R0-R10,LR}
+	
+	CMP R2, #97
+	BLEQ DRAW_A
+	
+	CMP R2, #98
+	BLEQ DRAW_B
+	
+	CMP R2, #99
+	BLEQ DRAW_C
+	
+	CMP R2, #100
+	BLEQ DRAW_D
+	
+	CMP R2, #101
+	BLEQ DRAW_E
+	
+	CMP R2, #102
+	BLEQ DRAW_F
+	
+	CMP R2, #103
+	BLEQ DRAW_G
+	
+	CMP R2, #104
+	BLEQ DRAW_H
+	
+	CMP R2, #105
+	BLEQ DRAW_I
+	
+	CMP R2, #106
+	BLEQ DRAW_J
+	
+	CMP R2, #107
+	BLEQ DRAW_K
+	
+	CMP R2, #108
+	BLEQ DRAW_L
+	
+	CMP R2, #109
+	BLEQ DRAW_M
+	
+	CMP R2, #110
+	BLEQ DRAW_N
+	
+	CMP R2, #111
+	BLEQ DRAW_O
+	
+	CMP R2, #112
+	BLEQ DRAW_P
+	
+	CMP R2, #113
+	BLEQ DRAW_Q
+	
+	CMP R2, #114
+	BLEQ DRAW_R
+	
+	CMP R2, #115
+	BLEQ DRAW_S
+	
+	CMP R2, #116
+	BLEQ DRAW_T
+	
+	CMP R2, #117
+	BLEQ DRAW_U
+	
+	CMP R2, #118
+	BLEQ DRAW_V
+	
+	CMP R2, #119
+	BLEQ DRAW_W
+	
+	CMP R2, #120
+	BLEQ DRAW_X
+	
+	CMP R2, #121
+	BLEQ DRAW_Y
+	
+	CMP R2, #122
+	BLEQ DRAW_Z
+	
+	CMP R2, #65
+	BLEQ DRAW_A_UPPER
+	
+	CMP R2, #66
+	BLEQ DRAW_B_UPPER
+	
+	CMP R2, #67
+	BLEQ DRAW_C_UPPER
+	
+	CMP R2, #68
+	BLEQ DRAW_D_UPPER
+	
+	CMP R2, #69
+	BLEQ DRAW_E_UPPER
+	
+	CMP R2, #70
+	BLEQ DRAW_F_UPPER
+	
+	CMP R2, #71
+	BLEQ DRAW_G_UPPER
+	
+	CMP R2, #72
+	BLEQ DRAW_H_UPPER
+	
+	CMP R2, #73
+	BLEQ DRAW_I_UPPER
+	
+	CMP R2, #74
+	BLEQ DRAW_J_UPPER
+	
+	CMP R2, #75
+	BLEQ DRAW_K_UPPER
+	
+	CMP R2, #76
+	BLEQ DRAW_L_UPPER
+	
+	CMP R2, #77
+	BLEQ DRAW_M_UPPER
+	
+	CMP R2, #78
+	BLEQ DRAW_N_UPPER
+	
+	CMP R2, #79
+	BLEQ DRAW_O_UPPER
+	
+	CMP R2, #80
+	BLEQ DRAW_P_UPPER
+	
+	CMP R2, #81
+	BLEQ DRAW_Q_UPPER
+	
+	CMP R2, #82
+	BLEQ DRAW_R_UPPER
+	
+	CMP R2, #83
+	BLEQ DRAW_S_UPPER
+	
+	CMP R2, #84
+	BLEQ DRAW_T_UPPER
+	
+	CMP R2, #85
+	BLEQ DRAW_U_UPPER
+	
+	CMP R2, #86
+	BLEQ DRAW_V_UPPER
+	
+	CMP R2, #87
+	BLEQ DRAW_W_UPPER
+	
+	CMP R2, #88
+	BLEQ DRAW_X_UPPER
+	
+	CMP R2, #89
+	BLEQ DRAW_Y_UPPER
+	
+	CMP R2, #90
+	BLEQ DRAW_Z_UPPER
+	
+	CMP R2, #48
+	BLEQ DRAW_0
+	
+	CMP R2, #49
+	BLEQ DRAW_1
+	
+	CMP R2, #50
+	BLEQ DRAW_2
+	
+	CMP R2, #51
+	BLEQ DRAW_3
+	
+	CMP R2, #52
+	BLEQ DRAW_4
+	
+	CMP R2, #53
+	BLEQ DRAW_5
+	
+	CMP R2, #54
+	BLEQ DRAW_6
+	
+	CMP R2, #55
+	BLEQ DRAW_7
+	
+	CMP R2, #56
+	BLEQ DRAW_8
+	
+	CMP R2, #57
+	BLEQ DRAW_9
+
+
+
+
+	POP {R0-R10,PC}
+	ENDFUNC
+		
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 DRAW_0
 	; Draws '0' starting from the top left corner x, y:
 	; R0: x
@@ -4678,3 +4888,5 @@ DRAW_Z
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+	
+	END
